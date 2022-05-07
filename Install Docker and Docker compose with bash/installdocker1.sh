bd5a8419b833dba311e632392cad955f062e178d
arynishere <95162220+arynishere@users.noreply.github.com>
2022-05-07 19:52:31 +0430

    Update installdocker1.sh

--- Install Docker and Docker compose with bash/installdocker1.sh
+++ Install Docker and Docker compose with bash/installdocker1.sh
@@ -35,6 +35,39 @@ if [[ $ans == docker ]];then
 	echo " Installation completed "
 
 	sleep 3 
+elif [[ $ans == DD ]];then
+echo " all right going install docker and compose :) "
+  sleep 2
+    rm -rf /etc/resolv.conf
+    cp ./resolv.conf /etc/
+    sudo apt-get remove docker docker-engine docker.io containerd runc -y
+
+    sudo apt-get update
+ sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+     echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+ echo " Installation completed ( docker )"
+ echo " start install compose ... "
+sleep 1
+
+	sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+	sudo chmod +x /usr/local/bin/docker-compose
+
+	docker-compose --version
+
+	echo " Installation completed (compose) "
+
+	sleep 3 
 elif [[ $ans == n ]];then
     echo -e "allreight"
 else 
