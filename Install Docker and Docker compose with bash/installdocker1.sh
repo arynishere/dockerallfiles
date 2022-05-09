241a707c06ec5b44ac8795deb24c3db4ae7bee13
arynishere <95162220+arynishere@users.noreply.github.com>
2022-05-09 11:39:10 +0430

    Update installdocker1.sh

--- Install Docker and Docker compose with bash/installdocker1.sh
+++ Install Docker and Docker compose with bash/installdocker1.sh
@@ -1,80 +1,66 @@
-#/bin/bash
-echo -e " Do you want install Docker or Docker-compose or both  ? (just install docker you should write name like docker,docker-compose or both type DD ) "
-read ans
-if [[ $ans == docker ]];then
-    echo " enjoy :)"
-    sleep 2
-    mv /etc/resolv.conf resolv.txt
-    cp ./resolv.conf /etc/
-    sudo apt-get remove docker docker-engine docker.io containerd runc -y
+function DNStoShecan() {
+  echo "changing your DNS temporarily  to shecan" 
+  mv /etc/resolv.conf /etc/resolv.conf.tmp
+  echo "nameserver 185.51.200.2" >> /etc/resolv.conf
+  echo "nameserver 178.22.122.100" >> /etc/resolv.conf
+}
 
-    sudo apt-get update
- sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y
+function normalDNS() {
+  echo "changing your DNS back to normal"
+  mv /etc/resolv.conf.tmp /etc/resolv.conf
+}
 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-     echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
- sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
- echo " Installation completed "
-	elif [[ $ans == docker-compose ]];then
-
-	echo " all right , start install docker-compose"
-
-	sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-	sudo chmod +x /usr/local/bin/docker-compose
-
-	docker-compose --version
-
-	echo " Installation completed "
-
-	sleep 3 
-elif [[ $ans == DD ]];then
-echo " all right going install docker and compose :) "
+function InstallDocker() {
+  echo "installing Docker ..."
   sleep 2
-    rm -rf /etc/resolv.conf
-    cp ./resolv.conf /etc/
     sudo apt-get remove docker docker-engine docker.io containerd runc -y
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl gnupg lsb-release -y
 
-    sudo apt-get update
- sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y
-
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-     echo \
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
- sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
- echo " Installation completed ( docker )"
- echo " start install compose ... "
-sleep 1
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
-	sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  echo "Done! you can use docker now"
+}
 
-	sudo chmod +x /usr/local/bin/docker-compose
+function InstallCompose(){
+  echo "Install Docker compose ..."
+  echo " all right , start install docker-compose"
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+  docker-compose --version
 
-	docker-compose --version
+        echo " Installation completed "
 
-	echo " Installation completed (compose) "
-       
-        rm /etc/resolv.conf       
-        
-        mv /etc/resolv.txt resolv.conf
-        
-        
-	sleep 3 
-elif [[ $ans == n ]];then
+        sleep 3
+}
+
+echo -n  " Do you want install Docker or Docker-Compose or both  ? (docker for Docker - docker-compose for Docker-Compose - DD for both):  "
+read ans
+
+
+if [[ $ans == docker ]]; then
+   DNStoShecan
+   InstallDocker
+   normalDNS
+
+  elif [[ $ans == docker-compose ]]; then
+   DNStoShecan
+   InstallCompose
+   normalDNS
+
+  elif [[ $ans == DD ]]; then
+  DNStoShecan
+  InstallDocker
+  InstallCompose
+  normalDNS
+
+  elif [[ $ans == n ]];then
     echo -e "all right"
-else 
-echo " please enter y or n (yes or no) "
+else
+echo " Please enter DD , docker , docker-compose or If you do not want to write n ) "
 fi
